13e0ded6393f316465ade9442877c117d4cb4270
Dhruvi <dhruvityagi04@gmail.com>
2024-12-05 10:05:45 +0530

    Initial Commit

---
+++ HelloCry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloCry
+//
+//  Created by Batch - 2 on 05/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
